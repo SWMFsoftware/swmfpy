5b3c7cb47e3f19ba3d2ca8f73f6baa34c59558ec
Qusai Al Shidi <qusai@umich.edu>
2020-03-03 09:47:40 -0500

    Fixed swmfpy module docstring to be better

--- swmfpy/__init__.py
+++ swmfpy/__init__.py
@@ -1,5 +1,10 @@
-"""A collection of tools to read, write, visualize with the
-   Space Weather Modeling Framework (SWMF)
+"""Tools for SWMF
+
+swmfpy
+======
+
+A collection of tools to read, write, visualize with the
+Space Weather Modeling Framework (SWMF)
 """
 __author__ = "Qusai Al Shidi"
 __license__ = "MIT"
