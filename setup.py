322b68666723bbed13b271fe5bae1fe0824d6c65
Qusai Al Shidi <me@qalshidi.science>
2020-11-10 11:27:46 -0500

    MAINT: Add UoM credit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ swmfpy.
 import setuptools
 
 
-with open('README.markdown') as fh_readme:
+with open('README.md') as fh_readme:
     LONG_DESCRIPTION = fh_readme.read()
 
 with open('requirements.txt') as fh_requirements:
@@ -13,7 +13,7 @@ with open('requirements.txt') as fh_requirements:
 
 setuptools.setup(
     name='swmfpy',
-    version='2020.9',
+    version='2020.10',
     author='Qusai Al Shidi',
     author_email='qusai@umich.edu',
     description='''A collection of tools for the Space Weather Modelling
