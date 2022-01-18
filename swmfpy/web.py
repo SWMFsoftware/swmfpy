e705f86c2cb8c8f633be415765fd1d78bda7377d
Qusai Al Shidi <me@qalshidi.science>
2022-01-18 10:44:05 -0500

    FIX: lru_cache explicit maxsize for 12 months

--- swmfpy/web.py
+++ swmfpy/web.py
@@ -235,7 +235,7 @@ def get_omni_data(time_from, time_to, **kwargs):
     return return_data
 
 
-@lru_cache
+@lru_cache(maxsize=12)
 def _download_static_page(url):
     """Downloads a cached webpage as a list of lines
 
